b8e0a05d7046520b424205045aa7d9e29d18c76f
yaginumatti <ryohei122yagi@gmail.com>
2025-08-12 23:44:05 +0900

    fix: Update ddl-auto property and ensure env loading

--- spring-app/src/main/java/com/example/harvestapp/HarvestAppApplication.java
+++ spring-app/src/main/java/com/example/harvestapp/HarvestAppApplication.java
@@ -5,14 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import io.github.cdimascio.dotenv.Dotenv;
 
-
 @SpringBootApplication
 public class HarvestAppApplication {
 
 	public static void main(String[] args) {
-				Dotenv.configure().filename(".env.local").systemProperties().load(); // Load .env.local file as system properties
+		if (System.getenv("RENDER") != null) {
+			// Render 環境では何もせずに環境変数から読む
+		} else {
+			// ローカルでは .env.local を読む
+			Dotenv.configure().filename(".env.local").systemProperties().load();
+		}
 		SpringApplication.run(HarvestAppApplication.class, args);
 	}
 
 }
-
