dc9a34ca97b424e98537be84c1ff62b341d2a3e9
Ryohei Yaginuma <55869046+yagi469@users.noreply.github.com>
2025-08-12 15:32:03 +0900

    Update entrypoint.sh

--- spring-app/entrypoint.sh
+++ spring-app/entrypoint.sh
@@ -1,25 +1,10 @@
 #!/bin/sh
 
-# ---
-# 確実なデバッグのために、変数が渡されているか再度確認する
-# ---
-echo "--- ENTRYPOINT SCRIPT: CHECKING ENVIRONMENT VARIABLES ---"
-echo "PORT: ${PORT}"
-echo "DATASOURCE URL: ${SPRING_DATASOURCE_URL}"
-echo "DATASOURCE USERNAME: ${SPRING_DATASOURCE_USERNAME}"
-# パスワードはログに出力しないのがベストプラクティス
-if [ -n "$SPRING_DATASOURCE_PASSWORD" ]; then
-    echo "DATASOURCE PASSWORD: [SET]"
-else
-    echo "DATASOURCE PASSWORD: [NOT SET]"
-fi
-echo "--- STARTING APPLICATION ---"
+# This script is no longer needed for debugging.
+# We will now directly execute the application with command-line arguments.
 
-# ---
-# アプリケーションを、システムプロパティとして変数を渡して実行する
-# ---
-exec java -Dserver.port=${PORT} \
-          -Dspring.datasource.url=${SPRING_DATASOURCE_URL} \
-          -Dspring.datasource.username=${SPRING_DATASOURCE_USERNAME} \
-          -Dspring.datasource.password=${SPRING_DATASOURCE_PASSWORD} \
-          -jar harvest-app-0.0.1-SNAPSHOT.jar
+exec java -jar harvest-app-0.0.1-SNAPSHOT.jar \
+     --server.port=${PORT} \
+     --spring.datasource.url=${SPRING_DATASOURCE_URL} \
+     --spring.datasource.username=${SPRING_DATASOURCE_USERNAME} \
+     --spring.datasource.password=${SPRING_DATASOURCE_PASSWORD}
