9712757cd0d178c866da54344c7c797f3ac0d947
Ryohei Yaginuma <55869046+yagi469@users.noreply.github.com>
2025-08-12 15:00:55 +0900

    Create entrypoint.sh

---
+++ spring-app/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# ---
+# 確実なデバッグのために、変数が渡されているか再度確認する
+# ---
+echo "--- ENTRYPOINT SCRIPT: CHECKING ENVIRONMENT VARIABLES ---"
+echo "PORT: ${PORT}"
+echo "DATASOURCE URL: ${SPRING_DATASOURCE_URL}"
+echo "DATASOURCE USERNAME: ${SPRING_DATASOURCE_USERNAME}"
+# パスワードはログに出力しないのがベストプラクティス
+if [ -n "$SPRING_DATASOURCE_PASSWORD" ]; then
+    echo "DATASOURCE PASSWORD: [SET]"
+else
+    echo "DATASOURCE PASSWORD: [NOT SET]"
+fi
+echo "--- STARTING APPLICATION ---"
+
+# ---
+# アプリケーションを、システムプロパティとして変数を渡して実行する
+# ---
+exec java -Dserver.port=${PORT} \
+          -Dspring.datasource.url=${SPRING_DATASOURCE_URL} \
+          -Dspring.datasource.username=${SPRING_DATASOURCE_USERNAME} \
+          -Dspring.datasource.password=${SPRING_DATASOURCE_PASSWORD} \
+          -jar target/harvest-app-0.0.1-SNAPSHOT.jar
